ffd972ddabc59a3c8459602fd46475178e5e25e6
Owen <Xiongwen.Ouyang@edu.sait.ca>
2026-09-05 18:31:20 -0600

    Initial Commit

---
+++ DORC/DORCApp.swift
@@ -0,0 +1,17 @@
+//
+//  DORCApp.swift
+//  DORC
+//
+//  Created by Owen on 9/5/26.
+//
+
+import SwiftUI
+
+@main
+struct DORCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DORCTests/DORCTests.swift
@@ -0,0 +1,19 @@
+//
+//  DORCTests.swift
+//  DORCTests
+//
+//  Created by Owen on 9/5/26.
+//
+
+import Testing
+@testable import DORC
+
+struct DORCTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
