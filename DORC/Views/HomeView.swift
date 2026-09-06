ab2181ecde54a04024c0f5838bff511dd4ec9253
Owen <Xiongwen.Ouyang@edu.sait.ca>
2026-09-06 00:11:01 -0600

    build header, add button and Quests description in homeview

--- DORC/Views/HomeView.swift
+++ DORC/Views/HomeView.swift
@@ -9,8 +9,80 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text("Homepage")
+        
+        NavigationStack{
+            ZStack{
+                LinearGradient(colors: [.blue.opacity(0.12),
+                                        .purple.opacity(0.18),.white], startPoint: .topLeading, endPoint: .bottomTrailing).ignoresSafeArea(edges: .all)
+                ScrollView{
+                    VStack(spacing: 60){
+                        welcomeSection
+                        questContent
+                    }
+                    .padding(.top,50)
+                }
+            }.toolbar {
+                ToolbarItem(placement: .principal){Image("dorc-typeface")
+                        .resizable()
+                        .scaledToFit()
+                    .frame(width: 90)}
+            }
+        }
     }
+    
+    private var welcomeSection: some View {
+        VStack(spacing: 24){
+            Text("Welcome back, Owen 👋")
+                .font(.headline)
+                .fontWeight(.bold)
+            
+            Button("Add Quests"){
+                print("Add quest")
+            }
+            .fontWeight(.semibold)
+            .foregroundStyle(.white)
+            .padding(.horizontal,20)
+            .padding(.vertical, 10)
+            .background(.blue.opacity(0.7))
+            .clipShape(Capsule())
+        }
+        .padding(.top, 36)
+        .padding(.bottom, 80)
+    }
+    
+    private var questContent: some View {
+        VStack {
+            sectionHeader(title:"Your Pesonal Quests")
+            sectionHeader(title:"DORC's Quests")
+                .padding(.bottom, 34)
+            Text("Quest cards")
+        }
+        .padding(.horizontal, 24)
+    }
+    
+    private func sectionHeader(title: String) -> some View {
+        HStack{
+            Text(title)
+                .font(.headline)
+                .fontWeight(.bold)
+            
+            Spacer()
+            
+            Button{
+                print("Add quest")
+            }label: {
+                HStack(spacing: 4){
+                    Text("View All")
+                    
+                    Image(systemName: "arror.right")
+                }
+            }
+            .foregroundStyle(.primary)
+        }
+    }
+    
+    
+    
 }
 
 #Preview {
--- DORC/Views/QuestsView.swift
+++ DORC/Views/QuestsView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct QuestsView: View {
     var body: some View {
-        Text("Quests")
+        Text("Quests Hello")
     }
 }
 
