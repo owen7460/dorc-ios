f55df0ae42597c8eb76c45d066c89653310210f7
Owen <Xiongwen.Ouyang@edu.sait.ca>
2026-09-07 11:10:47 -0600

    finish RanksView

--- DORC/Views/RanksView.swift
+++ DORC/Views/RanksView.swift
@@ -9,7 +9,41 @@ import SwiftUI
 
 struct RanksView: View {
     var body: some View {
-        Text("Ranks")
+        NavigationStack{
+            ZStack{
+                Color.blue.opacity(0.1).ignoresSafeArea(edges: .all)
+                
+                ScrollView{
+                    Text("LEADERBOARD")
+                        .font(.title)
+                        .fontWeight(.heavy)
+                        .padding(.bottom, 20)
+                    
+                    
+                    leader(number:1, name: "Leo", level: 3, xp: 120)
+                    leader(number:2, name: "Ava", level: 2, xp: 80)
+                    leader(number:3, name: "Nash", level: 1, xp: 0)
+                }
+            }.toolbar {
+                ToolbarItem(placement: .principal) {
+                    Image("dorc-typeface")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 90)
+                }
+            }
+        }
+    }
+    
+    private func leader
+    (number: Int,
+     name: String,
+     level: Int,
+     xp: Int) -> some View {
+        
+        HStack{
+            Text("\(number). \(name) - Level \(level) - \(xp) XP")
+        }.padding(.bottom, 4)
     }
 }
 
